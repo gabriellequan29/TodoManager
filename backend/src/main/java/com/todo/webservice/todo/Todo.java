9ce4ec8a2a971fce4a7a8e5592534151eb1f0ae4
Gabrielle Quan <qwy0129@gmail.com>
2022-01-23 12:54:48 -0500

    CRUD for Backend

--- backend/src/main/java/com/todo/webservice/todo/Todo.java
+++ backend/src/main/java/com/todo/webservice/todo/Todo.java
@@ -19,6 +19,8 @@ public class Todo {
         this.isCompleted = isCompleted;
     }
 
+    protected Todo() {}
+
     public long getId() {
         return id;
     }
--- backend/src/main/java/com/todo/webservice/todo/TodoController.java
+++ backend/src/main/java/com/todo/webservice/todo/TodoController.java
@@ -1,6 +1,7 @@
 package com.todo.webservice.todo;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,10 +16,20 @@ public class TodoController {
 
     /* Retrieve all Todos for a User
        GET /users/{user_name}/todos */
+
     @GetMapping("/users/{username}/todos")
     public List<Todo> getAllTodos(@PathVariable String username) {
         return todoHardcodedService.findAll();
     }
+
+     /* Retrieve single Todo for a User
+       GET /users/{user_name}/todos/{todo_id} */
+     @GetMapping("/users/{username}/todos/{id}")
+     public Todo getTodo(@PathVariable String username, @PathVariable long id) {
+         // Thread.sleep(3000);
+         return todoHardcodedService.findById(id);
+     }
+
 
     /* Delete a Todo of a User
        DELETE /users/{user_name}/todos/{todo_id} */
@@ -34,8 +45,20 @@ public class TodoController {
 
     /* Update a Todo
        PUT /users/{user_name}/todos/{todo_id} */
+    @PutMapping("/users/{username}/todos/{id}")
+    public ResponseEntity<Todo> updateTodo(@PathVariable String username,
+                                           @PathVariable long id, @RequestBody Todo todo) {
+        Todo updatedTodo = todoHardcodedService.save(todo);
+        return new ResponseEntity<Todo>(updatedTodo, HttpStatus.OK);
+    }
 
     /* Create a new Todo
        POST /users/{user_name}/todos */
+    @PostMapping("/users/{username}/todos/")
+    public ResponseEntity<Todo> createTodo(@PathVariable String username,
+                                           @RequestBody Todo todo) {
+        Todo newTodo = todoHardcodedService.save(todo);
+        return new ResponseEntity<Todo>(newTodo, HttpStatus.OK);
+    }
 
 }
